9d548029cea0c518d361c6861c02eda7a8ae41fd
rahimeD <duman032000@gmail.com>
2025-02-10 09:44:36 +0100

    Changed if in Kreditkonto for better output options

--- src/Kreditkonto.java
+++ src/Kreditkonto.java
@@ -13,15 +13,21 @@ public class Kreditkonto {
         if (betrag > 0) {
             kontostand += betrag;
             System.out.println(betrag + "€ eingezahlt. Neuer Kontostand: " + kontostand + "€");
+        } else {
+            System.out.println("Fehler: Der Betrag muss positiv sein.");
         }
     }
 
     public void abheben(double betrag) {
-        if (betrag > 0 && (kontostand - betrag) >= -kreditlimit) {
+        if (betrag <= 0) {
+            System.out.println("Fehler: Der Betrag muss größer als 0 sein.");
+            return;
+        }
+        if ((kontostand - betrag) >= -kreditlimit) {
             kontostand -= betrag;
             System.out.println(betrag + "€ abgehoben. Neuer Kontostand: " + kontostand + "€");
         } else {
-            System.out.println("Abhebung nicht möglich! Kreditlimit überschritten.");
+            System.out.println("Abhebung nicht möglich! Kreditlimit von " + kreditlimit + "€ überschritten.");
         }
     }
 
